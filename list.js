889afdfd1deed469d875ebcd4fd70e4930ac36a8
jatazoulja <jatazoulja@gmail.com>
2017-08-27 21:24:36 +0800

    initial commit

---
+++ list.js
@@ -0,0 +1,13 @@
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.create = function(event, context, callback) {
+    var response = {
+        statusCode: 200,
+        message: 'Success',
+        payload: event
+
+    };
+   callback(null, response)
+};
