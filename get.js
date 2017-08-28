48fd713df1de4564a38877dbdf64f7dceb38d720
jatazoulja <jatazoulja@gmail.com>
2017-08-28 17:47:04 +0800

    saving get

--- get.js
+++ get.js
@@ -1,21 +1,18 @@
 const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
 
-const dynamoDb = new AWS.DynamoDB();
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const resp = require('./lib/response.js');
 
 module.exports.main = function(event, context, callback) {
     var params = {
         TableName: process.env.DYNAMODB_TABLE,
         Key: {
-            apiId: {
-                S: event.pathParameters.id,
-            },
-            ownerId: {
-                S: event.requestContext.authorizer.claims.sub
-            }
+            apiId: event.pathParameters.id,
+            ownerId: event.requestContext.authorizer.claims.sub
+
         }
     };
-    dynamoDb.getItem(params, function(err, data) {
+    dynamoDb.get(params, function(err, data) {
         if (err) {
             callback(null, resp.failure({error: err, dump: params, event: event}));
         } else {
